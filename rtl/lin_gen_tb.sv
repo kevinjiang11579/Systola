d67e92bf865192c12aa20a108d8838f7ac395c59
ashment <gundamme@gmail.com>
2022-03-29 03:53:44 -0400

    tested generated linear arrangement

--- rtl/lin_gen_tb.sv
+++ rtl/lin_gen_tb.sv
@@ -4,7 +4,7 @@ module PE_TB;
 	reg clk, rstn, fire;
 	reg [7:0] w [0:3];
 	wire [11:0] o [0:3];
-    	reg [7:0] a;
+    	reg [7:0] a [0:3];
 	reg [7:0] cnt;
 
 	integer i;
@@ -19,17 +19,21 @@ module PE_TB;
 	end
 
 	PE_Lin_gen dut(.clk(clk), .rstn(rstn), .fire(fire), 
-		.in_w(w), .in_a(a), .outs(o));
+		.in_w(w), .in_a(a[0]), .outs(o));
 
 	initial begin
         $dumpfile("lin_gen_tb.vcd");
 		clk <= 0;
 		rstn <= 0;
-		a <= 1;
+		a[0] <= 1;
 		fire <= 1;
-        	for (i=0; i<4; i=i+1) begin
-    	        	w[i] <= i;
-        	end
+        	//for (i=0; i<4; i=i+1) begin
+    	        //	w[i] <= i;
+        	//end
+		w[0] <= 0;
+		w[1] <= 1;
+		w[2] <= 2;
+		w[3] <= 3;
 
 		$display("Initialized");
 
@@ -40,11 +44,11 @@ module PE_TB;
 			@(posedge clk);
 			$display("i=%d\tout=%d,%d,%d,%d",i,o[0],o[1],o[2],o[3]);
 
-			a <= a + 1;
+			a[0] <= a[0] + 1;
 		end
 		
 		#5;
-		a <= 0;
+		a[0] <= 0;
 		fire <= 0;
 
 		for (i=0; i<8; i=i+1) begin
